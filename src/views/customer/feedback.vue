b101959031c491f7940d6e645a6ddde636356f32
李晶晶 <1353281097@qq.com>
2020-06-02 11:34:14 +0800

    意见反馈页面完成

--- src/views/customer/feedback.vue
+++ src/views/customer/feedback.vue
@@ -1,31 +1,43 @@
 <template>
-    <div class="main_width" style="padding-left:10px;box-sizing: border-box;overflow: auto;">
-        <div class="user_rt_cont gl2-user_rt_cont wddd_d gl2-wddd_d">
-            <div class="top_title wddd_xx gl2-wddd_xx">
-                <strong>反馈意见</strong>
-            </div>
-        </div>
-        <div class="bg-white padding">
-            <el-form ref="form" :model="form" label-width="80px">
-                <el-form-item label="反馈意见">
-                    <el-input type="textarea" v-model="form.content" placeholder="请输入反馈意见" maxlength="500" show-word-limit :autosize="{ minRows: 5, maxRows: 10}"></el-input>
-                </el-form-item>
-                <el-form-item>
-                    <el-button type="primary" @click="onSubmit">提交</el-button> 
-                </el-form-item>
-            </el-form>
-        </div>
+  <div class="main_width customer">
+    <div class="gl2-wddd_d">
+      <div class="top_title">
+        <strong>反馈意见</strong>
+      </div>
     </div>
+    <div class="bg-white padding">
+      <el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="反馈意见">
+          <el-input
+            type="textarea"
+            v-model="form.content"
+            placeholder="请输入反馈意见"
+            maxlength="500"
+            show-word-limit
+            :autosize="{ minRows: 5, maxRows: 10}"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit" :disabled="disabled">提交</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 <script>
 import { isEmpty } from "@/utils";
 export default {
   data() {
     return {
-      form:{}
+      disabled:false,
+      form: {}
     };
   },
   created() {
+    if (!window.localStorage.getItem("paoce_token")) {
+      this.$router.push({ path: "/login" });
+      return;
+    }
   },
   methods: {
     onSubmit() {
@@ -34,18 +46,22 @@ export default {
         this.$message.error("请输入反馈意见");
         return;
       }
+      _this.disabled = true;
       this.$put("/api/user/feedback", {
-            content: _this.form.content
-        }).then(response => {
-            console.log(response);
-            if (response.code == 0) {
-              this.$message.success("反馈意见已提交成功");
-              _this.form.content = "";
-            }else{
-              this.$message.error(response.msg);
-            }
-        })
+        content: _this.form.content
+      }).then(response => { 
+        _this.disabled = false;
+        if (response.code == 0) {
+          _this.$message.success("反馈意见已提交成功");
+          _this.form.content = "";
+        } else {
+          _this.$message.error(response.msg);
+        }
+      });
     }
   }
 };
 </script>
+<style>
+@import "../../style/customer.css";
+</style>
