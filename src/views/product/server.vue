1f479ca14e6bf1602bcde50fbb51c59c646714c7
李晶晶 <1353281097@qq.com>
2020-06-03 18:27:10 +0800

    除了在线咨询 其他已完成

--- src/views/product/server.vue
+++ src/views/product/server.vue
@@ -2,8 +2,8 @@
   <div class="max_width organizationBox" id="server">
     <div class="organization">
       <div class="b-special-cc">
-        <img src="http://zljweb.com/images/tz-wz-2.png" width="534" height="25" alt="">
-        <p class="organization-wz"></p>
+        <img src="@/assets/icon/tz-wz-2.png" width="534" height="25" alt="">
+        <p class="organization-wz">{{info.mc}}</p>
         <div class="organization-wz gl12-organization-wz">
           <div class="gl12-organization-div" style="margin-left:20px">
             <span class="gl12-organization-span gl12-organization-i2">在线咨询</span>
@@ -14,33 +14,26 @@
     <div class="tzsb">
       <div class="tzsb-cc">
         <div class="jgzy">
-          <h3 :class=" type == '1' ? 'active':''" @click="goItem('1')">
-            <img src="http://zljweb.com/images/zs-2.png">
+          <h3 :class=" type == '1' ? 'active':''" @click="goItem('1')" >
+            <img src="@/assets/icon/zs-2.png">
             <p>资格证书 | 报告模版</p>
           </h3>
           <h3 :class=" type == '2' ? 'active':''" @click="goItem('2')">
-            <img src="http://zljweb.com/images/jg-2.png">
+            <img src="@/assets/icon/jg-2.png">
             <p>机构简介</p>
           </h3>
-          <h3 :class=" type == '3' ? 'active':''" @click="goItem('3')">
-            <img src="http://zljweb.com/images/lx-2.png">
-            <p>联系机构</p>
-          </h3>
-          <div class="tzsb-jj tzsb-jjwz gl1potal" style="display:block" v-if="type == '1'">
+          <div class="tzsb-jj tzsb-jjwz gl1potal" style="display:block" v-if=" type == '1'" >
             <el-row :gutter="10" style="padding:10px">
-              <el-col :span="6" v-for="(item,index) in list" :key="index">
+              <el-col :span="6" v-for="(item,index) in info.zhinfo" :key="index">
                 <div class="text-center margin-bottom">
                   <div style="overflow: hidden;">
-                    <img src="@/assets/2.png" alt="" style="width:80%;height:400px;margin:auto;">
+                    <img :src="item.ceImg" alt="" style="width:80%;height:400px;margin:auto;">
                   </div>
                 </div>
               </el-col>
             </el-row>
           </div>
           <div class="tzsb-jj tzsb-jjwz gl1potal" style="display:block" v-if="type == '2'">
-            <p v-html="info.intro"></p>
-          </div>
-          <div class="tzsb-jj tzsb-jjwz gl1potal" style="display:block" v-if="type == '3'">
             <p v-html="info.intro"></p>
           </div>
         </div>
@@ -66,10 +59,11 @@ export default {
     getInfo() {
       var _this = this;
       this.$fetch("/api/user/agencyInfo", { id: this.$route.query.id }).then(
-        response => {
-          console.log(response);
+        response => { 
           if (response.code == 0) {
-            _this.info = response.data;
+            _this.info = response.data; 
+          }else{
+            _this.$message.error(response.message)
           }
         }
       );
